7e98fc288813c96d04cf48f68486177592a87aad
Viral Shsatri <shastriv19@gmail.com>
2021-02-17 17:46:51 +0530

    Jedi-Level-7

---
+++ Jedi-Level-7/exercise2.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// Person Structure
+type Person struct {
+	name string
+}
+
+func changeMe(person *Person) {
+	person.name = "Shastri"
+}
+
+func main() {
+	person := Person{
+		name: "Viral",
+	}
+	fmt.Println(person)
+	changeMe(&person)
+	fmt.Println(person)
+}
